5fa2f039d97e181be8a87aa4f0f6a10c30d182a2
anderd7 <anderd7@unlv.nevada.edu>
2016-02-29 12:47:18 -0800

    Update 20160223204633_create_events.rb

--- db/migrate/20160223204633_create_events.rb
+++ db/migrate/20160223204633_create_events.rb
@@ -3,8 +3,8 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name
       t.string :description
-      t.string :start_date
-      t.string :end_date
+      t.datetime :start_date
+      t.datetime :end_date
     end
   end
 end
